5aa9b4b9c17c19b35fba9454911dded1a029bb3e
Ilin Maxim <il.maxim.in@gmail.com>
2025-10-02 18:26:26 +0700

    start to write class for work with log

--- Logs/Logs.py
+++ Logs/Logs.py
@@ -7,20 +7,35 @@ from dataclasses import dataclass, field
 class CurveItem():
     mnemonic: str
     unit: str = ""
+    value: str = ""
     description: str = ""
+    original_mnemonic: str = ""
     data: np.ndarray = field(default_factory=lambda: np.array([]))
 
-class CurveStorage():
-    def __init__(self, name_dataset = ""):
-        self.сurves = []
-        self._count_curve = 0
-        self._object_name = "Curve"
-        self._name_data_set = name_dataset
-    
+
+class CurveItemStorage():
+    def __init__(self, *args, name_dataset = "CurveStorange"):
+        self.name_data_set = name_dataset
+        self._mnemonic_unilites_curve = "Data" 
+
+        self.curves = []
+        self._list_name_mnemonic = []
+        
+        for curve in args:
+            if type(curve) != CurveItem:
+                continue
+            else:
+                self.curves.extend(args)
+                self._list_name_mnemonic.extend(args.mnemonic)
+
+
+
+    def __repr__(self):
+        return f"CurveItemStorage(name_dataset = {self.name_data_set})"
+        
     def __str__(self):
-        df = self.to_df()
-        return str(df)
-   
+        return f"{self.name_data_set}: {self._list_name_mnemonic}"
+    
     def __eq__(self, other):
         if not isinstance(other, type(self)):
             return False
@@ -31,55 +46,58 @@ class CurveStorage():
         if self._count_curve == 0:
             return True
 
-        return self.сurves == other.сurves
+        return self.curves == other.curves
 
     def __ne__(self, other):
         return not self.__eq__(other)
     
-    # def __getitem__(self, index):
-    #     if type(index) == int:
-    #         keys = list(self.objects)
-    #         index =  keys[index]
-    #     return self.objects[str(index)]
+    def __len__(self):
+        return self._count_object
     
-    # def __len__(self):
-    #     return self._count_object
+    def read_las(self, las_file):
+        print(type(las_file))
+        
+        def add_curves_from_lasioLASFile(self, las):
+            for i in range(len(las.curves())):
+                las_curve = las.curves[i]
+                mnemonic = las_curve.mnemonic
+                unit = las_curve.unit
+                value = las_curve.value
+                description = las_curve.descr
+                original_mnemonic = las_curve.original_mnemonic
+                data = las_curve.data
+                curve = CurveItem(mnemonic, unit, value, description, original_mnemonic,data)
+                print(curve)
+                self._list_name_mnemonic.append()
+                self.curves.append(curve)
+        
+
+        if type(las_file) == lasio.LASFile:
+            add_curves_from_lasioLASFile(self, las_file)
+        
+        else:
+            try:
+                las = lasio.read(las_file)
+                add_curves_from_lasioLASFile(self, las)
+            
+            except Exception as e:
+                print(e)
     
-    # def _test_name(self, name):
-    #     return name in self.objects
+    def to_df():
+        pass
 
-    # def add_objects(self, *args, object = None,  name = ''):
-    #     if not name:
-    #         name = self._object_name + "_" + str(self._count_object)
-        
-    #     repit_name = self._test_name(name)
-    #     count_repit_name = 0
-        
-    #     while repit_name:
-    #         count_repit_name += 1
-    #         name = f"{name}_{count_repit_name}"
-    #         repit_name = self._test_name(name)
-        
-    #     self.objects[name] = object
-    #     self._count_object += 1
-
-    # def to_df(self):        
-    #     if self._count_object == 0:
-    #         return pd.DataFrame(columns=["name"] + self._field_class_names)
-        
-    #     data = []
-    #     for name, obj in self.objects.items():
-    #         row = [name] + [getattr(obj, f.name) for f in fields(obj)]
-    #         data.append(row)
-        
-    #     df = pd.DataFrame(data, columns=["name"] + self._field_class_names)
-    #     return df
+    def to_exel():
+        pass
 
 las = lasio.read(r"C:\Users\User7\Desktop\pa-13_WBS.las")
 print(las.curves["CMW_MAX_TEN"])
 CMW_MAX_TEN = las.curves["CMW_MAX_TEN"]
  
 t = CurveItem("CMW_MAX_TEN", unit = CMW_MAX_TEN.unit, description = CMW_MAX_TEN.descr, data = CMW_MAX_TEN.data)
-set = CurveStorage()
-set.сurves.append(t)
-print(set)
+
+data_set = CurveItemStorage()
+data_set.read_las(las)
+
+print(data_set)
+
+
